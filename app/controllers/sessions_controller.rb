3c62e5246792431563c9736fe665ebadb16d1e06
Alex <alex.coder1@gmail.com>
2017-01-19 13:24:30 +0300

    Login page done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,23 @@
 class SessionsController < ApplicationController
   def new
-    @user = User.new
   end
 
   def create
-    @user = User.find_by email: params[:email]
+    @user = User.find_by email: attributes[:email]
 
-    if @user && @user.authenticate(params[:password])
+    if @user && @user.authenticate(attributes[:password])
       sign_in(@user)
+
+      redirect_to root_path
     else
-      render action: :new
+      flash[:error] = true
+      redirect_to action: :new
     end
+  end
+
+  private
+
+  def attributes
+    params.require('session').permit(:email, :password)
   end
 end
